12e1053bc508c467bebc1e6fde1e1940f2e2baf8
Tomás Agú <u99tomas@gmail.com>
2023-10-05 00:16:02 -0300

    feature: creates handlers and domain classes

---
+++ src/Core/Application/Company/Teams/CreateTeamRequest.cs
@@ -0,0 +1,29 @@
+﻿namespace Ticket360.Application.Company.Teams;
+
+using Domain.Company;
+
+public class CreateTeamRequest : IRequest<Guid>
+{
+    public string Name { get; set; }
+}
+
+public class CreateTeamHandler : IRequestHandler<CreateTeamRequest, Guid>
+{
+    private readonly IRepository<Team> _repository;
+
+    public CreateTeamHandler(IRepository<Team> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<Guid> Handle(CreateTeamRequest request, CancellationToken cancellationToken)
+    {
+        var team = new Team(request.Name);
+
+        await _repository.AddAsync(team, cancellationToken);
+
+        return team.Id;
+    }
+}
+
+
---
+++ src/Core/Domain/Company/Team.cs
@@ -0,0 +1,12 @@
+﻿namespace Ticket360.Domain.Company;
+
+public class Team : AuditableEntity, IAggregateRoot
+{
+    public string Name { get; private set; }
+
+    public Team(string name)
+    {
+        Name = name;
+    }
+}
+
